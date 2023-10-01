6433d9ea5d679b62703e047071b461a62eedcb3e
Hamza Habibullah <Habibullahamza@gmail.com>
2023-10-01 23:27:01 +0100

    working on responsive navbar for mobile screens

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,22 +1,32 @@
-// import React from 'react'
+import { useState } from 'react'
 import './Nav.css'
 import logo from '../../Assets/logo4.png'
 import { Link } from 'react-scroll'
+import { GiHamburgerMenu } from 'react-icons/gi'
+import { GrClose } from 'react-icons/gr'
 
 function Nav() {
+
+    const [toggle, setToggle] = useState(false)
+    const handleclick = () => {
+        setToggle(!toggle)
+    }
     return (
         <nav>
-        <div className='logo'>
-            <img src={logo} alt="" />
-        </div>
-        <ul className='menu'>
-            <Link to="Hero" spy={true} smooth={false} offset={-150} duration={200}>HOME</Link>
-            <Link to="About" spy={true} smooth={false} offset={-110} duration={200}>ABOUT</Link>
-            <Link to="skill" spy={true} smooth={false} offset={-120} duration={200}>SKILLS</Link>
-            <Link to="Projects" spy={true} smooth={false} offset={-110} duration={200}>PROJECTS</Link>
-            <Link to="Contact" spy={true} smooth={false} offset={-110} duration={200}>CONTACT</Link>
-        </ul>
-    </nav>
+            <div className='logo'>
+                <img src={logo} alt="" />
+            </div>
+            <ul className='menu'>
+                <Link to="Hero" spy={true} smooth={false} offset={-150} duration={200}>HOME</Link>
+                <Link to="About" spy={true} smooth={false} offset={-110} duration={200}>ABOUT</Link>
+                <Link to="skill" spy={true} smooth={false} offset={-110} duration={200}>SKILLS</Link>
+                <Link to="Projects" spy={true} smooth={false} offset={-110} duration={200}>PROJECTS</Link>
+                <Link to="Contact" spy={true} smooth={false} offset={-110} duration={200}>CONTACT</Link>
+            </ul>
+            <div className='menu-icon' onClick={handleclick}>
+                <GiHamburgerMenu size={30} />
+            </div>
+        </nav>
     )
 }
 

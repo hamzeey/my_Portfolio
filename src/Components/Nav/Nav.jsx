e1d7fbbfc472146c88903c4826c1b17d3fba070d
Hamza Habibullah <Habibullahamza@gmail.com>
2023-10-03 22:59:14 +0100

    Responsive nav complete

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -17,7 +17,7 @@ function Nav() {
                 <img src={logo} />
             </div>
 
-            <ul className={`menu ${toggle ? 'active' : ''}`}>
+            <ul className={`menu ${toggle ? 'activate' : 'inactive'}`}>
 
                 <Link to="Hero" spy={true} smooth={false} offset={-150} duration={200}>HOME</Link>
                 <Link to="About" spy={true} smooth={false} offset={-110} duration={200}>ABOUT</Link>
@@ -25,8 +25,9 @@ function Nav() {
                 <Link to="Projects" spy={true} smooth={false} offset={-110} duration={200}>PROJECTS</Link>
                 <Link to="Contact" spy={true} smooth={false} offset={-110} duration={200}>CONTACT</Link>
             </ul>
+
             <div className='menu-icon' onClick={handleclick}>
-                {toggle ? <GrClose size={30} /> : <GiHamburgerMenu size={30} />}
+                {toggle ? <GrClose size={35} /> : <GiHamburgerMenu size={35} />}
             </div>
         </nav>
     )

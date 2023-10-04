0e26e07adf91c310e4abbf250f7f8bf5d8dc32ee
Hamza Habibullah <Habibullahamza@gmail.com>
2023-10-04 21:53:28 +0100

    Start fo hero section

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,11 +1,35 @@
 // import React from 'react'
 import './Hero.css'
-
+import { BsLinkedin } from 'react-icons/bs'
+import { AiFillGithub } from 'react-icons/ai'
+import { BsWhatsapp } from 'react-icons/bs'
+import { BsInstagram } from 'react-icons/bs'
+import { RiTwitterXLine } from 'react-icons/ri'
 
 function Hero() {
     return (
         <div className='Hero'>
-            HERO SEC
+            <div className='hero-container'>
+                <div className='socials'>
+
+                    <div><a href="" target='_blank'> <BsLinkedin size={15} /> </a></div>
+                    <div><a href="" target='_blank'> <AiFillGithub size={15} /> </a></div>
+                    <div><a href="" target='_blank'> <RiTwitterXLine size={15} /> </a></div>
+                    <div><a href="" target='_blank'> <BsWhatsapp size={15} /> </a></div>
+                    <div><a href="" target='_blank'> <BsInstagram size={15} /> </a></div>
+
+                </div>
+                <div className='hero-txt'>
+
+                </div>
+                <div className='hero-image'>
+
+                </div>
+                <div className='scroll-down'>
+                    <h4>SCROLL DOWN</h4>
+                </div>
+
+            </div>
         </div>
     )
 }

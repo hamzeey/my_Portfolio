4441fe81749a4d34dee0e084b07c98049d2c2be9
Hamza Habibullah <Habibullahamza@gmail.com>
2023-10-05 21:06:53 +0100

    update to Hero section

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -5,6 +5,7 @@ import { AiFillGithub } from 'react-icons/ai'
 import { BsWhatsapp } from 'react-icons/bs'
 import { BsInstagram } from 'react-icons/bs'
 import { RiTwitterXLine } from 'react-icons/ri'
+import ME from '../../Assets/PNG image 12.png'
 
 function Hero() {
     return (
@@ -12,21 +13,21 @@ function Hero() {
             <div className='hero-container'>
                 <div className='socials'>
 
-                    <div><a href="" target='_blank'> <BsLinkedin size={15} /> </a></div>
-                    <div><a href="" target='_blank'> <AiFillGithub size={15} /> </a></div>
-                    <div><a href="" target='_blank'> <RiTwitterXLine size={15} /> </a></div>
-                    <div><a href="" target='_blank'> <BsWhatsapp size={15} /> </a></div>
-                    <div><a href="" target='_blank'> <BsInstagram size={15} /> </a></div>
+                    <div><a href="" target='_blank'> <BsLinkedin size={18} /> </a></div>
+                    <div><a href="" target='_blank'> <AiFillGithub size={18} /> </a></div>
+                    <div><a href="" target='_blank'> <RiTwitterXLine size={18} /> </a></div>
+                    <div><a href="" target='_blank'> <BsWhatsapp size={18} /> </a></div>
+                    <div><a href="" target='_blank'> <BsInstagram size={18} /> </a></div>
 
                 </div>
                 <div className='hero-txt'>
 
                 </div>
                 <div className='hero-image'>
-
+                    <img src={ME} />
                 </div>
                 <div className='scroll-down'>
-                    <h4>SCROLL DOWN</h4>
+                    <a href="#Contact">SCROLL DOWN</a>
                 </div>
 
             </div>

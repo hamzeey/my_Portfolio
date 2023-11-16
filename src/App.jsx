27eae28be1be48f071d8e7694f9a84f37eccf1c5
Hamza Habibullah <Habibullahamza@gmail.com>
2023-11-16 10:51:41 +0100

    bug fixes in About section

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import './App.css'
 import Hero from './Components/Hero/Hero'
 import About from './Components/About/About'
+import Skill from './Components/Skill/Skill'
 import Nav from './Components/Nav/Nav'
 import Contact from './Components/Contact/Contact'
 import Footer from './Components/Footer/Footer'
 import Projects from './Components/Projectss/Projects'
-import Skill from './Components/Skill/Skill'
+
 
 function App() {
-
-
   return (
     <>
       <Nav />
